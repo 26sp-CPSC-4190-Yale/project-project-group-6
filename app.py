bfb0a85d563fb7f46ba2ed7113ab2b413af1de7c
Short Aaliyah <as4496@zoo.cs.yale.edu>
2026-04-10 23:50:37 -0400

    user auth

--- app.py
+++ app.py
@@ -1,8 +1,49 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import pandas as pd
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user, UserMixin
+from flask_sqlalchemy import SQLAlchemy
+
+############################################################
+#database stuff
+db = SQLAlchemy() # get db reference
+
+# define user model class
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+  
+    def __repr__(self):
+        return f'{self.username}'
+
+############################################################
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev-key'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+# init database
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+# qssign Login View
+login_manager = LoginManager()
+# redirect to the login page if authentication fails
+login_manager.login_view = "login"
+login_manager.init_app(app)
+
+# query user from the database based on user ID
+@login_manager.user_loader
+def load_user(user_id):
+    # Query user from database based on user ID
+    return db.session.get(User, int(user_id))
+
+
+############################################################
+# dataframe stuff
 df = pd.read_csv("Most-Recent-Cohorts-Institution.csv", low_memory=False)
 
 columns_needed = [
@@ -27,6 +68,7 @@ df = df[columns_needed].copy()
 for col in ["ADM_RATE", "UGDS", "LOCALE", "NPT4_PUB", "NPT4_PRIV", "MD_EARN_WNE_P10"]:
     df[col] = pd.to_numeric(df[col], errors="coerce")
 
+############################################################
 
 def classify_size(ugds):
     if pd.isna(ugds):
@@ -179,17 +221,83 @@ def get_recommendations(answers, limit=5):
 
     return results
 
+############################################################
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/takequiz")
+@login_required
+def takequiz():
+    return render_template("quiz.html")
 
 @app.route("/recommend", methods=["POST"])
+@login_required
 def recommend():
     answers = request.get_json()
     recommendations = get_recommendations(answers)
     return jsonify(recommendations)
+
+#apply login functionality
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if user.password ==  password:
+                login_user(user, remember=True)
+                return redirect(url_for('home'))
+            else:
+                return "Incorrect password."
+        else:
+            return "Email does not exist."
+    return render_template("login.html", user=current_user)
+
+# register user
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password1 = request.form.get("password")
+        password2 = request.form.get("password2")
+
+        email_exists = User.query.filter_by(email=email).first()
+        username_exists = User.query.filter_by(username=username).first()
+        # validation
+        if email_exists:
+            return "Email is already in use."
+        elif username_exists:
+            return "Username is already in use."
+        elif password1 != password2:
+            return "Passwords don't match!"
+        elif len(username) < 2:
+            return "Username is too short."
+        elif len(password1) < 6:
+            return "Password is too short."
+        elif len(email) < 4:
+            return "Email is not valid."
+        else:
+            new_user = User(email=email, username=username, password=password1)
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            login_user(new_user, remember=True)
+            return redirect(url_for('home'))
+
+    return render_template("signup.html", user=current_user)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
